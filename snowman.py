4478b356c5af5894b209b43e7b4258fa3afe3713
Linda Ebbert <linda_ebbert@yahoo.de>
2026-05-15 23:47:37 +0200

    Moved ASCII-Art to a separate file

--- snowman.py
+++ snowman.py
@@ -1,36 +1,9 @@
 import random
+from ascii_art import STAGES
 
 # List of secret words
 WORDS = ["python", "git", "github", "snowman", "meltdown"]
-# Snowman ASCII Art stages
-STAGES = [
-     # Stage 0: Full snowman
-     """
-      ___  
-     /___\\ 
-     (o o) 
-     ( : ) 
-     ( : ) 
-     """,
-     # Stage 1: Bottom part starts melting
-     """
-      ___  
-     /___\\ 
-     (o o) 
-     ( : ) 
-     """,
-     # Stage 2: Only the head remains
-     """
-      ___  
-     /___\\ 
-     (o o) 
-     """,
-     # Stage 3: Snowman completely melted
-     """
-      ___  
-     /___\\ 
-     """
- ]
+
 
 def get_random_word():
     """Selects a random word from the list."""
